4426ac483fb8341d053e6d7edde41495d43cb96f
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-11-24 21:50:28 -0300

    feat: initial settings

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from 'react-native';
+import { styles } from './styles';
+
+export const Groups = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Groups</Text>
+    </SafeAreaView>
+  );
+};
