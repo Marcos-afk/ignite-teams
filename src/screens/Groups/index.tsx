5da9e44902ea5b272f199e280d549ddeb456c1a1
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-02 23:48:37 -0300

    feat: listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,19 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { mock } from './mock';
 import * as Styled from './styles';
 
 export const Groups = () => {
+  const [groups, setGroups] = useState<string[]>(mock);
+
   return (
     <Styled.Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Nome da turma" />
+      <FlatList data={groups} keyExtractor={(item) => item} renderItem={({ item }) => <GroupCard title={item} />} />
     </Styled.Container>
   );
 };
---
+++ src/screens/Groups/mock.ts
@@ -0,0 +1,1 @@
+export const mock = ['Turma de futebol', 'Turma de handebol', 'Turma de basebol', 'Turma de xadrez'];
