528953de9d02798e8fd783263d02eea11816dcff
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-02 21:57:14 -0300

    feat: adding icon library and deploying it

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import * as Styled from './styles';
 import logoImg from '@assets/logo.png';
+import { HeaderProps } from './HeaderProps';
 
-export const Header = () => {
+export const Header = ({ showBackButton }: HeaderProps) => {
   return (
     <Styled.Container>
+      {showBackButton && (
+        <>
+          <Styled.BackButton>
+            <Styled.BackIcon />
+          </Styled.BackButton>
+        </>
+      )}
       <Styled.Logo source={logoImg} />
     </Styled.Container>
   );
