4460601da5886ef7102e365cf2a07523154ce995
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-07 05:38:09 -0300

    feat: listing the filters

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter/indext';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { FlatList } from 'react-native';
 import * as Styled from './styles';
 
 export const Players = () => {
+  const [team, setTeam] = useState('');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Styled.Container>
       <Header showBackButton />
@@ -16,8 +21,17 @@ export const Players = () => {
         <ButtonIcon icon="add" />
       </Styled.Form>
 
-      <Filter text="Turma A" isActive />
-      <Filter text="Turma B" />
+      <Styled.HeaderList>
+        <FlatList
+          data={['Time A', 'Time B', 'Time C', 'Time D', 'Time E']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <Filter text={item} isActive={item === team} onPress={() => setTeam(item)} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+
+        <Styled.NumberOfPlayers>{players.length}</Styled.NumberOfPlayers>
+      </Styled.HeaderList>
     </Styled.Container>
   );
 };
