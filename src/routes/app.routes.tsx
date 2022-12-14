1ec3f154ddcec4d464dfcd6741d65d1801c454a9
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-14 19:49:15 -0300

    feat: implementing stack navigator

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,16 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Groups } from '@screens/Groups';
+import { NewGroups } from '@screens/NewGroups';
+import { Players } from '@screens/Players';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export const AppRoutes = () => {
+  return (
+    <Navigator>
+      <Screen name="groups" component={Groups} />
+      <Screen name="new-groups" component={NewGroups} />
+      <Screen name="players" component={Players} />
+    </Navigator>
+  );
+};
