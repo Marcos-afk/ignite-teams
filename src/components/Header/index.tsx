90b554bd0e5d7af3e8f05c2ca403e73fe005cd46
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-12-15 04:23:07 -0300

    feat: browsing the application

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import * as Styled from './styles';
 import logoImg from '@assets/logo.png';
 import { HeaderProps } from './HeaderProps';
+import { useNavigation } from '@react-navigation/native';
 
 export const Header = ({ showBackButton }: HeaderProps) => {
+  const { navigate } = useNavigation();
+
+  const handleGoBack = () => {
+    navigate('groups');
+  };
+
   return (
     <Styled.Container>
       {showBackButton && (
         <>
-          <Styled.BackButton>
+          <Styled.BackButton onPress={handleGoBack}>
             <Styled.BackIcon />
           </Styled.BackButton>
         </>
--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -2,9 +2,16 @@ import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { useNavigation } from '@react-navigation/native';
 import * as Styled from './styles';
 
 export const NewGroups = () => {
+  const { navigate } = useNavigation();
+
+  const handleNew = () => {
+    navigate('players', { group: 'Rocket' });
+  };
+
   return (
     <Styled.Container>
       <Header showBackButton />
@@ -12,7 +19,7 @@ export const NewGroups = () => {
         <Styled.Icon />
         <Highlight title="Nova Turma" subtitle="crie uma turma para adicionar pessoas" />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Styled.Content>
     </Styled.Container>
   );
